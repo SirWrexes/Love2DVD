62050d48351fe8075ba448ef4eb83752e0926e82
Ludovic Fernandez <ludofernandez@msn.com>
2025-04-11 14:09:43 +0200

    refactor(debug): Rename global `DEBUG` → `__DEBUG`

--- game/src/debug.lua
+++ game/src/debug.lua
@@ -1,14 +1,15 @@
-DEBUG = false
+__DEBUG = false
+__DEV = false
 
 if os.getenv "LOCAL_LUA_DEBUGGER_VSCODE" == "1" then
     for _, v in ipairs(arg) do
         if v == "-debug" then
-            DEBUG = true
+            __DEBUG = true
             break
         end
     end
 
-    if DEBUG then
+    if __DEBUG then
         local lldebugger = require "lldebugger"
 
         function love.errorhandler(msg)
