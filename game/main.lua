3dd81e01d644f2a8f2635c51a5c6157a2a663269
Ludovic Fernandez <ludofernandez@msn.com>
2025-04-11 16:15:23 +0200

    refactor(lib.inspect): Add `inspect.proc`
    Processor function table that can be extended at any time, and attached
    to `inspect` for ease of access anywhere it is used.

--- game/main.lua
+++ game/main.lua
@@ -20,6 +20,13 @@ function love.load()
     local r, g, b = Util.hexToRgb(bgColour)
     love.graphics.setBackgroundColor(love.math.colorFromBytes(r, g, b))
     logo:init()
+
+    ---Remove all metatables and [`ExtendableObject`](lua://ExtendableObject) shenanigans from output
+    function inspect.proc.NO_META(item, path)
+        local i = #path
+        if path[i] == inspect.METATABLE or path[i] == "__instanceof" then return end
+        return item
+    end
 end
 
 function love.update(delta)
@@ -33,21 +40,16 @@ function love.update(delta)
 
     ctx.delta = delta
     ctx.mouse.pos:set(love.mouse.getPosition())
-    ctx.window.dim:set(love.graphics.getHeight(), love.graphics.getWidth())
-    logo:update(ctx)
-end
+    ctx.window.dim:set(love.graphics.getDimensions())
 
-local function nometa(item, path)
-    local i = #path
-    if path[i] == inspect.METATABLE or path[i] == "__instanceof" then return end
-    return item
+    logo:update(ctx)
 end
 
 function love.draw()
     logo:draw(ctx)
 
     if __DEV then
-        love.graphics.print("Ctx: " .. inspect(ctx, { process = nometa }), 10, 10)
-        love.graphics.print("Logo: " .. inspect(logo, { process = nometa }), 250, 10)
+        love.graphics.print("Ctx: " .. inspect(ctx, { process = inspect.proc.NO_META }), 10, 10)
+        love.graphics.print("Logo: " .. inspect(logo, { process = inspect.proc.NO_META }), 250, 10)
     end
 end
