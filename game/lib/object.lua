2052b753d732c3c1f13f57cd24f1132c3f4289e5
Ludovic Fernandez <ludofernandez@msn.com>
2025-04-11 14:01:16 +0200

    refactor: Improve `object.lua`
    - `object` → `Object`
    - `__getinstance()` → `__makeinstance()`
        - Function code is more readable
        - Sets the call signature of crated instances to `self:new(...)`
    - `___instanceof` → `__instanceof` (-1 underscore)
    - snake_case replaced with camelCase to better fit LÖVE's style
    - Add `Object.couldBeObject(v: unknown): v is ExtendableObject`

--- game/lib/object.lua
+++ game/lib/object.lua
@@ -1,6 +1,7 @@
 --
 -- Project: lua-object
 -- objects for Lua
+-- Edited by Sir Wrexes
 -- Original source: https://github.com/nusov/lua-object/blob/master/src/lua-object/object.lua
 --
 -- Copyright 2015 Alexander Nusov. Licensed under the MIT License.
@@ -8,39 +9,61 @@
 --
 --
 
----@class Object
-local object = {}
+---@class ExtendableObject
+---@field private __index      self
+---@field private __instanceof self
+---@field private __call       fun(...) : self
+local Object = {}
 
-function object:__getinstance()
-	local o = setmetatable({ ___instanceof = self }, self)
-	self.__index = self
-	return o
+---@private
+function Object:__makeinstance()
+    local base = { __instanceof = self }
+    local instance = setmetatable(base, self)
+
+    self.__index = self
+    self.__call = function(self, ...)
+        return self:new(...)
+    end
+
+    return instance
 end
 
-function object:init(...) end
+function Object:init(...) end
 
-function object:new(...)
-	local o = self:__getinstance()
-	o:init(...)
-	return o
+function Object:new(...)
+    local o = self:__makeinstance()
+    o:init(...)
+    return o
 end
 
-function object:extend(...)
-	local cls = self:__getinstance()
-	cls.init = function() end
+function Object:extend(...)
+    local cls = self:__makeinstance()
+    cls.init = function() end
 
-	for _, f in pairs({ ... }) do
-		f(cls, self)
-	end
-	return cls
+    for _, f in pairs { ... } do
+        f(cls, self)
+    end
+
+    return cls
 end
 
-function object.is_typeof(instance, class)
-	return instance ~= nil and (instance.___instanceof == class)
+---@protected
+---@param var unknown
+function Object.couldBeObject(var)
+    return var ~= nil and type(var) == "table"
 end
 
-function object.is_instanceof(instance, class)
-	return instance ~= nil and (instance.___instanceof == class or object.is_instanceof(instance.___instanceof, class))
+---@param instance unknown
+---@param class ExtendableObject
+function Object.isTypeof(instance, class)
+    return Object.couldBeObject(instance) and (instance.__instanceof == class)
 end
 
-return object
+---@param instance unknown
+---@param class ExtendableObject
+function Object.isInstanceof(instance, class)
+    return Object.couldBeObject(instance)
+        and (instance.__instanceof == class or Object.isInstanceof(instance.__instanceof, class))
+end
+
+return Object
